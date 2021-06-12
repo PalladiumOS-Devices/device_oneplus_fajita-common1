srgrusso/android_device_oneplus_sdm845-common

sdm845-common: Introduce DeviceExtras package!
- Adds a system settings panel for:

* DC-Dimming
* HBM & Auto HBM
* Panel Modes (sRGB, DCI-P3 Wide Colour Gamut & Wide Colour mode)
* Ambient display gestures (doze merged into DeviceExtras)
* FPS Overlay
* Alert slider configs
* Vibrator strength preferences  (System, Call & Notification)
* Drop redundant doze overlay & enable tri-state-switch UI

- All resources have been imported locally from packages/resources/devicesettings:
* LineageOS/android_packages_resources_devicesettings@ab954fb
* LineageOS/android_packages_resources_devicesettings@cc1ad81

Notes!
- This does not include KeyHandler support for client_package_name!
- Make sure to set config_alertSliderIntent in overlay to broadcast alert slider intents to the system for a bugless Alert Slider UI:
* If your rom does not have support, here's a reference: https://github.com/Evolution-X/frameworks_base/commits/elle/packages/SystemUI/src/com/android/systemui/tristate/TriStateUiControllerImpl.java

Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Adrian DC <radian.dc@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Co-authored-by: deadmanxxd <deadmanxxd@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/evolution/device/DeviceExtras/AutoHBMThresholdPreference.java
@@ -1,0 +1,57 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+public class AutoHBMThresholdPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 20000;
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, "20000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/FPSTileService.java
@@ -1,0 +1,65 @@
+/**
+ * Copyright (C) 2020 Omni ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceExtras;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (org.evolution.device.DeviceExtras.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, org.evolution.device.DeviceExtras.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/HBMModeSwitch.java
@@ -1,0 +1,40 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+
+public class HBMModeSwitch {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/PanelModeTileService.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+@TargetApi(24)
+public class PanelModeTileService extends TileService {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Intent panelModes = new Intent(this, PanelSettingsActivity.class);
+        panelModes.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(panelModes);
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/PanelSettingsActivity.java
@@ -1,0 +1,64 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+public class PanelSettingsActivity extends Activity {
+    private PanelSettings mPanelSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mPanelSettingsFragment = new PanelSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mPanelSettingsFragment)
+                .commit();
+        } else {
+            mPanelSettingsFragment = (PanelSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/WideModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class WideModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_wide_color_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

PalladiumOS-Devices/device_oneplus_fajita-common1

Co-authored-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: durgesh33 <dgagoria3@gmail.com>

DeviceExtras/src/com/palladium/device/DeviceExtras/AutoHBMThresholdPreference.java
@@ -1,0 +1,57 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.palladium.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+public class AutoHBMThresholdPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 20000;
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, "20000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceExtras/src/com/palladium/device/DeviceExtras/FPSTileService.java
@@ -1,0 +1,65 @@
+/**
+ * Copyright (C) 2020 Omni ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palladium.device.DeviceExtras;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (com.palladium.device.DeviceExtras.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, com.palladium.device.DeviceExtras.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+}

DeviceExtras/src/com/palladium/device/DeviceExtras/HBMModeSwitch.java
@@ -1,0 +1,40 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.palladium.device.DeviceExtras;
+
+import android.content.Context;
+
+public class HBMModeSwitch {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+}

DeviceExtras/src/com/palladium/device/DeviceExtras/PanelModeTileService.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.palladium.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+@TargetApi(24)
+public class PanelModeTileService extends TileService {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Intent panelModes = new Intent(this, PanelSettingsActivity.class);
+        panelModes.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(panelModes);
+    }
+}

DeviceExtras/src/com/palladium/device/DeviceExtras/PanelSettingsActivity.java
@@ -1,0 +1,64 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.palladium.device.DeviceExtras;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+public class PanelSettingsActivity extends Activity {
+    private PanelSettings mPanelSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mPanelSettingsFragment = new PanelSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mPanelSettingsFragment)
+                .commit();
+        } else {
+            mPanelSettingsFragment = (PanelSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceExtras/src/com/palladium/device/DeviceExtras/WideModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.palladium.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class WideModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_wide_color_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}